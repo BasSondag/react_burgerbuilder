38a992a28308e55e73fcea4f63e110d5f7468c57
Bas Sondag <bassondag@Bass-MacBook-Pro.local>
2020-07-14 16:43:54 -0700

    Create:bacic part

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import classes from './Burger.css';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+
+const burger = (props) => {
+  let transforemdIngredients = Object.keys(props.ingredients)
+    .map(igKey => {
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        return <BurgerIngredient key={igKey+i} type={igKey} />
+      } );
+    } )
+      .reduce((arr, el) => {
+        return arr.concat(el)
+      }, []);
+  if(transforemdIngredients.length === 0) {
+    transforemdIngredients = <p>Please start adding ingredients.</p>
+  }
+  return (
+    <div className={classes.Burger}>
+      <BurgerIngredient type='bread-top'/>
+      {transforemdIngredients}
+      <BurgerIngredient type='bread-bottom'/>
+    </div>
+  );
+};
+
+export default burger;
